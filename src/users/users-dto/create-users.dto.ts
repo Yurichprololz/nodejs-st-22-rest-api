cc3987208e72aa9fce412a5f7083b7a6d972d33e
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-07-17 19:42:52 +0300

    feat: add basic validation

--- src/users/users-dto/create-users.dto.ts
+++ src/users/users-dto/create-users.dto.ts
@@ -1,5 +1,25 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
+
 export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly login: string;
+
+  @IsString()
+  @Matches(/\d.+?[a-z]|[a-z].+?\d/i, {
+    message: 'The password must contains number and figure',
+  })
   readonly password: string;
+
+  @IsNumber()
+  @Min(4)
+  @Max(130)
   readonly age: number;
 }
