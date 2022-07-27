ca3195a69d2f60b9c8ba83c1689a96733ef8099c
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-07-27 19:57:04 +0300

    feat: configure app to work with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/model/user.model';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot()],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [User],
+      autoLoadModels: true,
+      define: {
+        timestamps: false,
+        createdAt: false,
+        updatedAt: false,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      // synchronize: false,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
