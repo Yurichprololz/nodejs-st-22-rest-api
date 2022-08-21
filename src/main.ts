a5c773baa13e84d3084563636b708f2a551cd291
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-08-21 15:04:20 +0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
